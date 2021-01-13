7d87572a38bc1607dad7c06551047e36ec99b3c0
100528339BC <100528339@schools.edin.org>
2021-01-13 14:24:45 +0000

    completed program

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner; 
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter first name");
+    String forename = scan.next();
+
+    System.out.println("Enter surname");
+    String surname = scan.next();
+
+    System.out.println("Enter your mother's maiden name");
+    String mother = scan.next();
+
+    System.out.println("Enter the Name of the city you were born");
+    String city = scan.next();
+
+    String starwarsName = surname.substring(0,3) + forename.substring(0,2) + " " + mother.substring(0,3) + city.substring(0,2);
+
+    System.out.println("your starwars name is "+ starwarsName);
+
   }
-}
+} 
